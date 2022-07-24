4543254251dd7893c531e660b27546e2a1d98957
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 18:52:51 -0500

    Arrays

---
+++ Introduction C#/Arrays/Arrays/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] coffeTypes;
+float[] coffePrices;
+coffeTypes = new string[] { "Expresso", "Largo", "Filtrado" };
+coffePrices = new float[] { 1.2f, 1.5f, 5.0f };
+for (int i = 0; i < coffeTypes.Length; i++)
+    Console.WriteLine(coffeTypes[i] + "Coffe $" + coffePrices[i]);
--- Introduction C#/LogicalOperators/LogicalOperators/Program.cs
+++ Introduction C#/LogicalOperators/LogicalOperators/Program.cs
@@ -14,3 +14,4 @@ Console.WriteLine("The result of the logical OR is: " + result2);
 // not
 bool result3 = !value1;
 Console.WriteLine("The result of the logical NOT is: " + result3);
+    
