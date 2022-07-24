9fc2b2fc428843553bb8941288c519a277ca84dc
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 17:15:50 -0500

    string exapmle

---
+++ Introduction C#/stringExample2/stringExample2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace stringExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What´s your name? Please write your full name: ");
+            string fullUserName = Console.ReadLine();
+            Console.WriteLine("Welcome "+fullUserName);
+        }
+    }
+}
