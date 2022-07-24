22672a84eac8ac5d77457e1c260531551f69be1b
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 18:22:59 -0500

    numeric operators

---
+++ Introduction C#/SquareArea/SquareArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float sideA = 10.235468f;
+            float sideB = 20;
+
+            float area = sideA * sideB;
+            Console.WriteLine(area);
+        }
+    }
+}
