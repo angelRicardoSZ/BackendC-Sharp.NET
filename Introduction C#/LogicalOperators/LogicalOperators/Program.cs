7dc0378be2545f302ea343e532e496de90f1f845
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 18:34:47 -0500

    Logical operators

---
+++ Introduction C#/LogicalOperators/LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool value1 = true;
+bool value2 = false;
+
+// and
+bool result = value1 && value2;
+Console.WriteLine("The result of the logical AND is: "+result);
+
+// or 
+bool result2 = value1 || value2;
+Console.WriteLine("The result of the logical OR is: " + result2);
+
+
+// not
+bool result3 = !value1;
+Console.WriteLine("The result of the logical NOT is: " + result3);
