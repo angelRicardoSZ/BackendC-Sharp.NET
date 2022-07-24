4d2e0431e136fed00072fa55a5dacd3f3eb61c9e
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 17:52:02 -0500

    String managment

---
+++ Introduction C#/StringManagment/StringManagment/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int height = 170;
+int age = 26;
+string name = "Angel Ricardo";
+string placeHolderText = "The information you requested is: \n";
+string information = placeHolderText + "user name " + name + ", user age " 
+        + age + ", user height " + height;
+Console.WriteLine(information);
