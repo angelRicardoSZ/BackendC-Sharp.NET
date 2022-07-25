ff4e2ce37e33f6eee0878098e82a3439747969f9
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-25 18:35:22 -0500

    projects and libraries

---
+++ dotNetBasics/consoleProject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+Console.WriteLine("Ingrese nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Ingrese cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Ingrese edad en años");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords()} años");
