3d04aaadc50eaab357895787dc5cb8a72d352745
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-24 19:29:33 -0500

    lists

---
+++ Introduction C#/Lists/Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> shoppingList = new List<string>();
+shoppingList.Add("Item 1");
+shoppingList.Add("Item 2");
+shoppingList.Add("Item 3");
+shoppingList.Add("Item 1");
+
+for (int i = 0; i < shoppingList.Count; i++)
+{ 
+    Console.WriteLine(shoppingList[i]);
+
+}
+
+shoppingList.Remove("Item 1");
+shoppingList.RemoveAt(1);
+
+for (int i = 0; i < shoppingList.Count; i++)
+{
+    Console.WriteLine(shoppingList[i]);
+        
+}
+
