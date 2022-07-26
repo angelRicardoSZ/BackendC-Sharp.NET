aa264c7ebe62ee94b4776c95384375c6cd406fa7
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-26 14:24:23 -0500

    Adding courses

---
+++ POO/FirstStep/Entidades/TiposJornada.cs
@@ -0,0 +1,7 @@
+namespace CoreEscuela.Entidades
+{
+    public enum TiposJornada
+    {
+        Manana, Tarde, Noche
+    }
+}
--- POO/FirstStep/Program.cs
+++ POO/FirstStep/Program.cs
@@ -3,4 +3,23 @@ using CoreEscuela.Entidades;
 
 var escuela = new Escuela("school1",2022,TiposEscuela.Primaria,pais:"MX", ciudad:"Pue");
 
+var curso1 = new Curso() {
+    Nombre="101",
+};
+
+var curso2 = new Curso() {
+    Nombre="201",
+};
+
+var curso3 = new Curso() {
+    Nombre="301",
+};
+
 Console.WriteLine(escuela);
+System.Console.WriteLine("-----------");
+Console.WriteLine(curso1.Nombre + ", "+ curso1.UniqueId);
+
+Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}" );
+
+Console.WriteLine(curso3);
+
