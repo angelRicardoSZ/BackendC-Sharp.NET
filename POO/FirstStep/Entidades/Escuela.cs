8018dbb540ea29d4520bd23b9e3c76234c923c7b
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-26 21:07:29 -0500

    Entity framework: creating models

--- POO/FirstStep/Entidades/Escuela.cs
+++ POO/FirstStep/Entidades/Escuela.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace CoreEscuela.Entidades
 {
     class Escuela
@@ -16,7 +17,7 @@ namespace CoreEscuela.Entidades
 
         public TiposEscuela TipoEscuela{get;set;}
         
-        public Curso[] Cursos {get;set;}
+        public List<Curso> Cursos {get;set;}
 
         public Escuela(string nombre, int annio) => (Nombre, AnnioCreacion) = (nombre,annio);
 
--- POO/FirstStep/Program.cs
+++ POO/FirstStep/Program.cs
@@ -1,14 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CoreEscuela.Entidades;
+using System.Collections.Generic;
 using static System.Console;
 
 var escuela = new Escuela("school1",2022,TiposEscuela.Primaria,pais:"MX", ciudad:"Pue");
 
-escuela.Cursos = new Curso[] {
-    new Curso(){Nombre = "101"},
-    new Curso(){Nombre = "201"},
-    new Curso(){Nombre = "301"}
+escuela.Cursos = new List<Curso>(){
+    new Curso(){Nombre = "101", Jornada = TiposJornada.Manana},
+    new Curso(){Nombre = "201", Jornada = TiposJornada.Manana},
+    new Curso(){Nombre = "301", Jornada = TiposJornada.Manana}
 };
+
+escuela.Cursos.Add(new Curso(){Nombre = "102", Jornada = TiposJornada.Noche});
+escuela.Cursos.Add(new Curso(){Nombre = "202", Jornada = TiposJornada.Noche});
+
+var otraColeccion = new List<Curso>(){
+    new Curso(){Nombre = "401", Jornada = TiposJornada.Manana},
+    new Curso(){Nombre = "501", Jornada = TiposJornada.Manana},
+    new Curso(){Nombre = "502", Jornada = TiposJornada.Manana}
+};
+
+escuela.Cursos.AddRange(otraColeccion);
 
 ImprimirCursosEscuela(escuela);
 
@@ -19,7 +31,7 @@ static void ImprimirCursosEscuela(Escuela escuela)
     WriteLine("Cursos de la escuela");
     WriteLine("-----------");
 
-    if( escuela?.Cursos != null)
+    if( escuela?.Cursos != null) 
     {
         foreach (var curso in escuela.Cursos)
         {
