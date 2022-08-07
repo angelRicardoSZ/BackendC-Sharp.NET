524eaccfa0c27ba639cceb217793462365febfb6
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-08-06 19:42:35 -0500

    class 3: working with arrays, lists and list of classes

---
+++ HectorLeonCourse/class3/ArrayAndLists/ArrayAndLists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAndLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            List<int> lista = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            foreach(var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            
+        }
+    }
+}
