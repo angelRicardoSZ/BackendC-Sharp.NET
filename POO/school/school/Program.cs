fbcec77458ab9414ceb933e45346aadb133fc8d3
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-08-09 21:56:02 -0500

    starting the project school

---
+++ POO/school/school/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace school
+{
+    internal class Program
+    {   
+        class CoreEscuela
+        {
+            public string nombre;
+
+            public string direccion;
+
+        }
+        static void Main(string[] args)
+        {
+            var miEscuela = new CoreEscuela();
+
+            miEscuela.nombre = "School1";
+
+            miEscuela.direccion = "street";
+
+
+
+
+        }
+    }
+}
